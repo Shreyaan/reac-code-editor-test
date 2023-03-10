c62905211653e65e9f7db6880073533666b9631a
shreyaan <Shreyaans41@gmail.com>
2023-03-10 23:17:37 +0530

    .

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,40 +6,24 @@ import Editor from "@monaco-editor/react";
 import { useLocalStorage } from "usehooks-ts";
 
 function IFrameDemo() {
-  // const [code, setCode] = useState(`<!DOCTYPE html>
-  //   <html lang="en">
-  //   <head>
-  //       <meta charset="UTF-8">
-  //       <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  //       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  //       <title>Document</title>
-  //   </head>
-  //   <body>
-  //       <h1>Hello World</h1>
-  //       <script>
-  //           // create alert
-  //           alert("Hello World");
-  //       </script>
-  //   </body>
-  //   </html>`);
   const [code, setCode] = useLocalStorage(
     "code",
     `<!DOCTYPE html>
-  //   <html lang="en">
-  //   <head>
-  //       <meta charset="UTF-8">
-  //       <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  //       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  //       <title>Document</title>
-  //   </head>
-  //   <body>
-  //       <h1>Hello World</h1>
-  //       <script>
-  //           // create alert
-  //           alert("Hello World");
-  //       </script>
-  //   </body>
-  //   </html>`
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <h1>Hello World</h1>
+        <script>
+            // create alert
+            // alert("Hello World");
+        </script>
+    </body>
+    </html>`
   );
   const outputRef = useRef(null) as unknown as { current: HTMLIFrameElement };
 
@@ -65,6 +49,7 @@ function IFrameDemo() {
         <Editor
           height="100%"
           defaultLanguage="html"
+          
           onChange={handleEditorChange}
           value={code}
         />
